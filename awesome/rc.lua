52c8b98b2152bc9fa61b4aa8a8f500124a30be61
Justin Endacott <justin.endacott@gmail.com>
2013-02-21 14:50:03 +1000

    Remove tabbex in favor of tmux

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -41,7 +41,7 @@ end
 beautiful.init("/usr/share/awesome/themes/default/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
-terminal = "urxvt"
+terminal = 'urxvt -e zsh -c "tmux -q has-session && exec tmux attach-session -d || exec tmux new-session -n$USER -s$USER@$HOSTNAME"'
 editor = os.getenv("EDITOR") or "vim"
 editor_cmd = terminal .. " -e " .. editor
 
