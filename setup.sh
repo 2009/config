1661e7142fd995a47a7222f1f9b0c359fc955df6
Justin Endacott <justin.endacott@gmail.com>
2017-09-07 01:00:55 +1000

    Automatically switch to audio device when connected

--- setup.sh
+++ setup.sh
@@ -8,16 +8,17 @@ echo "Creating link files"
 
 [ -d $HOME/.config ] || mkdir $HOME/.config
 
-ln -sn $HOME/config/termite   $HOME/.config/termite
-ln -sn $HOME/config/awesome   $HOME/.config/awesome
-ln -sn $HOME/config/zshrc     $HOME/.zshrc
-ln -sn $HOME/config/zprofile  $HOME/.zprofile
-ln -sn $HOME/config/oh-my-zsh $HOME/.oh-my-zsh
-ln -sn $HOME/config/vim       $HOME/.vim
-ln -sn $HOME/config/vimrc     $HOME/.vimrc
-ln -sn $HOME/config/ranger    $HOME/.config/ranger
-ln -sn $HOME/config/dircolors $HOME/.dircolors
-ln -sn $HOME/config/weechat   $HOME/.weechat
+ln -sn $HOME/config/termite          $HOME/.config/termite
+ln -sn $HOME/config/awesome          $HOME/.config/awesome
+ln -sn $HOME/config/zshrc            $HOME/.zshrc
+ln -sn $HOME/config/zprofile         $HOME/.zprofile
+ln -sn $HOME/config/oh-my-zsh        $HOME/.oh-my-zsh
+ln -sn $HOME/config/vim              $HOME/.vim
+ln -sn $HOME/config/vimrc            $HOME/.vimrc
+ln -sn $HOME/config/ranger           $HOME/.config/ranger
+ln -sn $HOME/config/dircolors        $HOME/.dircolors
+ln -sn $HOME/config/weechat          $HOME/.weechat
+ln -sn $HOME/config/pulse/default.pa $HOME/.config/pulse/default.pa
 
 echo "Installing vim bundles"
 
