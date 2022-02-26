fb0591ab202c3bebeb20eaa9da3695d20d2903df
Justin Endacott <justin.endacott@gmail.com>
2022-02-26 14:50:43 +1000

    Add spacemacs config

--- setup.sh
+++ setup.sh
@@ -20,12 +20,13 @@ ln -sn $HOME/config/awesome          $HOME/.config/awesome
 ln -sn $HOME/config/zshrc            $HOME/.zshrc
 ln -sn $HOME/config/zprofile         $HOME/.zprofile
 ln -sn $HOME/config/oh-my-zsh        $HOME/.oh-my-zsh
-ln -sn $HOME/config/vim              $HOME/.vim
-ln -sn $HOME/config/vimrc            $HOME/.vimrc
+#ln -sn $HOME/config/vim              $HOME/.vim
+#ln -sn $HOME/config/vimrc            $HOME/.vimrc
 ln -sn $HOME/config/ranger           $HOME/.config/ranger
 ln -sn $HOME/config/dircolors        $HOME/.dircolors
-ln -sn $HOME/config/weechat          $HOME/.weechat
-ln -sn $HOME/config/pulse/default.pa $HOME/.config/pulse/default.pa
+ln -sn $HOME/config/spacemacs        $HOME/.spacemacs
+#ln -sn $HOME/config/weechat          $HOME/.weechat
+#ln -sn $HOME/config/pulse/default.pa $HOME/.config/pulse/default.pa
 
 echo "Installing vim bundles"
 
@@ -37,8 +38,8 @@ function command_installed {
   command -v $1 >/dev/null 2>&1 || { echo "Please install $1 and rerun. Aborting." >&2; exit 1; }
 }
 
-command_installed cmake
-command_installed python3
-cd $HOME/config/vim/bundle/YouCompleteMe
-./install.py
+#command_installed cmake
+#command_installed python3
+#cd $HOME/config/vim/bundle/YouCompleteMe
+#./install.py
 
